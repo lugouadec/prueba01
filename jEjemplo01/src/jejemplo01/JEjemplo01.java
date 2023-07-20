135c92b15e92e074ab41d4d04e2814c40cb03bab
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2023-07-19 20:33:42 -0600

    proyecto inicial, primer commit

---
+++ jEjemplo01/src/jejemplo01/JEjemplo01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jejemplo01;
+
+/**
+ *
+ * @author LugoUAdeC
+ */
+public class JEjemplo01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Holas Mundo !!!");
+    }
+    
+}

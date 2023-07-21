f4e7cb3b907c8d06f2308eec33a57d39d8224a4c
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2023-07-20 18:34:04 -0600

    cambios

---
+++ jEjemplo01/src/modelo/Alumnos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author LugoUAdeC
+ */
+public class Alumnos {
+
+    private int numero;
+    
+    public Alumnos() {
+        numero=0;
+    
+    }
+    
+    public void setNumero(int n){
+        numero=n;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+
+    
+}

46eb3e9eb1c9a413719524cf3621facefb199884
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2023-07-20 18:35:28 -0600

    cambios 2

--- jEjemplo01/src/jejemplo01/JEjemplo01.java
+++ jEjemplo01/src/jejemplo01/JEjemplo01.java
@@ -4,6 +4,8 @@
  */
 package jejemplo01;
 
+import modelo.Alumnos;
+
 /**
  *
  * @author LugoUAdeC
@@ -16,6 +18,14 @@ public class JEjemplo01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Holas Mundo !!!");
+        System.out.println("Esto es una pago !!!");
+        System.out.println("Esto es todo lo que pueda");
+        
+        Alumnos  n = new Alumnos();
+        
+        n.setNumero(20);
+        System.out.println(n.getNumero());
+        
     }
     
 }
